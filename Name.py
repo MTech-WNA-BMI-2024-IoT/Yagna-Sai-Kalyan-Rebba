f3e0665b6c2b3e889fa31b6c375184c71fdd48dc
YSKR99 <86254142+YSKR99@users.noreply.github.com>
2024-02-23 12:32:38 +0530

    Update Name.py

--- Name.py
+++ Name.py
@@ -2,22 +2,8 @@ import RPi.GPIO as GPIO
 from RPLCD.i2c import CharLCD
 import time
 
-# Define LCD column and row size for 16x2 LCD.
-lcd_columns = 16
-lcd_rows = 2
-
-# Initialize LCD.
 lcd = CharLCD('PCF8574', 0x27)
-
-# Clear the LCD display.
 lcd.clear()
-
-# Print the message.
-message = "My name is\nYagna Sai Kalyan Rebba"
-lcd.write_string(message)
-
-# Wait for 5 seconds
+lcd.write_string("Hello, LCD!")
 time.sleep(5)
-
-# Clear the LCD display.
 lcd.clear()
