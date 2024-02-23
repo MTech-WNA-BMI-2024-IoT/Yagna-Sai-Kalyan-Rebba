fbc1e649fa20d5d56aafcb7c41ad7abbe0e72a6b
YSKR99 <86254142+YSKR99@users.noreply.github.com>
2024-02-23 11:59:47 +0530

    Update Name.py

--- Name.py
+++ Name.py
@@ -1,1 +1,21 @@
-print("My name is Yagna Sai Kalyan Rebba")
+import board
+import busio
+import adafruit_character_lcd.character_lcd_i2c as character_lcd
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows = 2
+
+# Initialize I2C bus.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Initialize LCD.
+lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns, lcd_rows)
+
+# Clear the LCD display.
+lcd.clear()
+
+# Print the message.
+message = "My name is\nYagna Sai Kalyan Rebba"
+lcd.message = message
+
