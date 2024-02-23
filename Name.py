833ab6176f4c146752b06d03d0675960937310b1
YSKR99 <86254142+YSKR99@users.noreply.github.com>
2024-02-23 12:18:10 +0530

    Update Name.py

--- Name.py
+++ Name.py
@@ -1,21 +1,22 @@
-import board
-import busio
-import adafruit_character_lcd.character_lcd_i2c as character_lcd
+from gpiozero import CharacterLCD
+from gpiozero.tools import text
 
 # Define LCD column and row size for 16x2 LCD.
 lcd_columns = 16
 lcd_rows = 2
 
-# Initialize I2C bus.
-i2c = busio.I2C(board.SCL, board.SDA)
-
 # Initialize LCD.
-lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns, lcd_rows)
+lcd = CharacterLCD(rs=26, e=19, d4=13, d5=6, d6=5, d7=11, cols=lcd_columns, rows=lcd_rows)
 
 # Clear the LCD display.
 lcd.clear()
 
 # Print the message.
 message = "My name is\nYagna Sai Kalyan Rebba"
-lcd.message = message
+lcd.text = message
 
+# Wait for 5 seconds
+lcd.wait_for_text()
+
+# Clear the LCD display.
+lcd.clear()
