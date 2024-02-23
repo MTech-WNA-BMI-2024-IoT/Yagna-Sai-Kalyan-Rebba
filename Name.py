351e2b85982c1b35591e486fa80802fa16574971
YSKR99 <86254142+YSKR99@users.noreply.github.com>
2024-02-23 12:20:21 +0530

    Update Name.py

--- Name.py
+++ Name.py
@@ -1,22 +1,32 @@
-from gpiozero import CharacterLCD
-from gpiozero.tools import text
+from gpiozero import Device, MCP23017
+from RPLCD.i2c import CharLCD
+
+# Set up MCP23017 as a backend for gpiozero
+Device.pin_factory = MCP23017
 
 # Define LCD column and row size for 16x2 LCD.
 lcd_columns = 16
 lcd_rows = 2
 
+# Initialize MCP23017
+mcp = MCP23017()
+
 # Initialize LCD.
-lcd = CharacterLCD(rs=26, e=19, d4=13, d5=6, d6=5, d7=11, cols=lcd_columns, rows=lcd_rows)
+lcd = CharLCD(i2c_expander=mcp, address=0x27, port=1, cols=lcd_columns, rows=lcd_rows)
 
 # Clear the LCD display.
 lcd.clear()
 
 # Print the message.
 message = "My name is\nYagna Sai Kalyan Rebba"
-lcd.text = message
+lcd.write_string(message)
 
 # Wait for 5 seconds
-lcd.wait_for_text()
+lcd.cursor_pos = (0, 0)
+lcd.blink = True
+lcd.cursor_mode = 'hide'
+lcd.autoscroll = True
+lcd.write_string(' ' * (lcd_columns * lcd_rows))
 
 # Clear the LCD display.
-lcd.clear()
+lcd.close(clear=True)
