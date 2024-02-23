57e8015fb4e648739864e1afbbf9e368a9ad0ea2
YSKR99 <86254142+YSKR99@users.noreply.github.com>
2024-02-23 12:36:34 +0530

    Update Name.py

--- Name.py
+++ Name.py
@@ -3,7 +3,11 @@ from RPLCD.i2c import CharLCD
 import time
 
 lcd = CharLCD('PCF8574', 0x27)
-lcd.clear()
-lcd.write_string("Hello, LCD!")
-time.sleep(5)
-lcd.clear()
+
+try:
+    lcd.clear()
+    lcd.write_string("LCD is working!")
+    time.sleep(5)
+    lcd.clear()
+finally:
+    GPIO.cleanup()
