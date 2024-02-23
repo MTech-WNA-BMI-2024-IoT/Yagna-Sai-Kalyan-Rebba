3fe15f894567d042229a5c09d4c8432eb3f4f3ef
YSKR99 <86254142+YSKR99@users.noreply.github.com>
2024-02-23 12:24:05 +0530

    Update Name.py

--- Name.py
+++ Name.py
@@ -1,4 +1,3 @@
-import board
 import busio
 import adafruit_character_lcd.character_lcd_i2c as character_lcd
 
@@ -6,10 +5,10 @@ import adafruit_character_lcd.character_lcd_i2c as character_lcd
 lcd_columns = 16
 lcd_rows = 2
 
-# Initialize I2C bus.
+# Initialize I2C bus using Blinka.
 i2c = busio.I2C(board.SCL, board.SDA)
 
-# Initialize LCD with MCP23017 I2C expander.
+# Initialize LCD.
 lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns, lcd_rows, address=0x27)
 
 # Clear the LCD display.
